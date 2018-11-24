261262627dbc7ad7237ee9091ff75f5c09abbc37
borsukvasyl <v.borsuk2000@gmail.com>
2018-11-24 18:26:15 +0200

    added Callback import to callbacks package __init__.py file

--- deeprl/callbacks/__init__.py
+++ deeprl/callbacks/__init__.py
@@ -2,4 +2,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from deeprl.callbacks.Callback import Callback
 from deeprl.callbacks.EGreedyDecay import EGreedyDecay
