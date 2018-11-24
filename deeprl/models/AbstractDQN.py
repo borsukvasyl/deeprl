1815737b81de82780f0032139aa7c051e2df443d
borsukvasyl <v.borsuk2000@gmail.com>
2018-11-24 18:18:42 +0200

    added AbstractDQN model

---
+++ deeprl/models/AbstractDQN.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractDQN(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def get_q(self, states):
+        pass
+
+    @abstractmethod
+    def train(self, batch_states, batch_actions, q_target):
+        pass
+
+    def get_one_q(self, state):
+        return self.get_q([state])[0]
